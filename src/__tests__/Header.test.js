ee1c489b973b576f8aa33a2edebdd61e7a549429
De65 <kyalodiana6@gmail.com>
2023-05-09 08:09:16 +0300

    completed assignment

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,15 +1,8 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import Header from "../components/Header";
+import React from "react";
 
-test("renders a <header> element", () => {
-  const { container } = render(<Header />);
-  expect(container.querySelector("header")).toBeInTheDocument();
-});
-
-test("renders a <h1> with the blog name", () => {
-  render(<Header name="Underreacted" />);
-  const h1 = screen.queryByText("Underreacted");
-  expect(h1).toBeInTheDocument();
-  expect(h1.tagName).toBe("H1");
-});
+function Header({name}) {
+  return <header>
+    <h1>{name}</h1>
+  </header>;
+}
+export default Header;
